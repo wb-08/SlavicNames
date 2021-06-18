c7f11991f8d22a0f26359d847532095ffaa70b4a
wb08 <wb-08>
2021-06-18 19:11:22 +0300

    parse gender

--- parse_fio.py
+++ parse_fio.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 fio_dict = {}
+gender_dict = {}
 conn = sqlite3.connect('db/fio.db')
 cursor = conn.cursor()
 
@@ -35,6 +36,32 @@ def search_in_midnames(midname):
         return True
 
 
+def define_gender_by_surname(surname):
+    cursor.execute("SELECT gender FROM surnames WHERE surname=?", ([surname.lower()]))
+    record = cursor.fetchone()
+
+    if record is None:
+        return False
+    else:
+        if record[0] is None:
+            return False
+        else:
+            return record[0]
+
+
+def define_gender_by_name(name):
+    cursor.execute("SELECT gender FROM names WHERE name=?", ([name.lower()]))
+    record = cursor.fetchone()
+
+    if record is None:
+        return False
+    else:
+        if record[0] is None:
+            return False
+        else:
+            return record[0]
+
+
 def find_fio(value):
     if search_in_midnames(value) is True:
         fio_dict.update({'Midname': value})
@@ -46,11 +73,39 @@ def find_fio(value):
         fio_dict.update({'Surname': value})
 
 
-def parse_fio(fio):
+def define_gender(fio, value):
+    gender_by_surname = define_gender_by_surname(value)
+    if gender_by_surname is False:
+        gender_by_name = define_gender_by_name(value)
+        if gender_by_name is False:
+            gender_dict.update({'fio': fio, 'gender:': 'u'})
+        else:
+            gender_dict.update({'fio': fio, 'gender:': gender_by_name})
+            return True
+    else:
+        gender_dict.update({'fio': fio, 'gender:': gender_by_surname})
+        return True
+
+
+def fio_parse(fio):
     fio_lst = fio.split(' ')
     for i in fio_lst:
         find_fio(i)
     return fio_dict
+
+
+def gender_parse(fio):
+    fio_lst = fio.split(' ')
+    for i in fio_lst:
+        if define_gender(fio, i):
+            break
+    return gender_dict
+
+
+
+
+
+
 
 
 
